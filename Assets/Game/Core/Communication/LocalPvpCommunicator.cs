439949da1ffdf95e54f872c3f40a365bf2ed0426
Maksym Kriukov <tbiwka@yandex.ru>
2023-10-03 09:18:31 +0300

    Local pvp provider

--- Assets/Game/Core/Communication/LocalPvpCommunicator.cs
+++ Assets/Game/Core/Communication/LocalPvpCommunicator.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace Core.Communication
 {
@@ -6,12 +7,43 @@ namespace Core.Communication
     {
         public UniTask<UserAttackScenarioState> GetAttackScenario()
         {
-            throw new System.NotImplementedException();
+            return UniTask.FromResult(UserAttackScenarioState.GetInitial());
         }
 
         public UniTask<UserBoardState> GetBoard()
         {
-            throw new System.NotImplementedException();
+            var size = new Vector2Int(10, 10);
+            var board = UserBoardState.GetInitial(size, "dummy");
+           
+            for (var y = 1; y < 9; y++)
+            {
+                for (var x = 1; x < 9; x++)
+                {
+                    var content = GameTileContentType.Empty;
+                    var rnd = Random.Range(0, 6);
+                    switch (rnd)
+                    {
+                        case 0:
+                            content = GameTileContentType.Wall;
+                            break;
+                        case 1:
+                            content = GameTileContentType.LaserTower;
+                            break;
+                        case 2:
+                            content = GameTileContentType.MortarTower;
+                            break;
+                        case 3:
+                            content = GameTileContentType.Ice;
+                            break;
+                        case 4:
+                            content = GameTileContentType.Lava;
+                            break;
+                    }
+
+                    board.Content[size.x * y + x] = content;
+                }
+            }
+            return UniTask.FromResult(board);
         }
     }
 }
